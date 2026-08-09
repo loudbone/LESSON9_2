edfe0517d067aa5018e0572c8d6962ff0585dff7
若杉京佑 <loudbone.k17@gmail.com>
2026-08-09 10:50:41 +0900

    ユーザー情報入力

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 import streamlit as st
-st.write("朝昼晩の挨拶が各ページに書いてあるよ。")
+st.title("ユーザー情報入力")
+if 'name' not in st.session_state:
+    st.session_state.name=""
+if 'grade' not in st.session_state:
+    st.session_state.grade=""
+if 'hobbies' not in st.session_state:
+    st.session_state.hobbies=""
+name=st.text_input("名前")
+grade=st.selectbox("学年",
+    ["","小5","小6","中1","中2","中3"])
+hobbies=st.multiselect("趣味",
+    ["読書","スポーツ","ゲーム","音楽","絵画","その他"])
+if st.button("情報を保存"):
+    st.session_state.name=name
+    st.session_state.grade=grade
+    st.session_state.hobbies=hobbies
+    st.success("情報を保存しました！！")            
